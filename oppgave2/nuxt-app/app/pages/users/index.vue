3b8666b653838efed642467e0a518e21679dac69
Sondre <sondrelb@outlook.com>
2026-02-04 15:52:30 +0100

    Made simple boilerplate for API page
    It fetches simple commands from server/api

--- oppgave2/nuxt-app/app/pages/users/index.vue
+++ oppgave2/nuxt-app/app/pages/users/index.vue
@@ -4,7 +4,7 @@
 
     <div class="text-black text-center text-4xl flex flex-col justify-center items-center gap-5">
       <input class="border-2 text-center w-[20vw] border-black rounded-3xl bg-white" v-model="userId" type="number" placeholder="Enter user ID" />
-      <button class="border-black border-2 rounded-2xl text-3xl p-2 bg-slate-600 text-gray-300 font-bold" @click="goToUser">Go to User Profile</button>
+      <button class="border-black border-2 rounded-2xl text-3xl p-2 bg-slate-600 text-white font-bold" @click="goToUser">Go to User Profile</button>
     </div>
   </div>
 </template>
